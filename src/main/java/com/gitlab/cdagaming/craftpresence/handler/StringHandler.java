c6eab07d25cf11d9992c81bbf095c6c3f04fb0f0
cdagaming <cstack2011@yahoo.com>
2019-03-19 18:15:51 -0500

    [Fix] Deprecate WordUtils in exchange for in-house method
    FINAL PRIMER I PROMISE

--- src/main/java/com/gitlab/cdagaming/craftpresence/handler/StringHandler.java
+++ src/main/java/com/gitlab/cdagaming/craftpresence/handler/StringHandler.java
@@ -3,7 +3,6 @@ package com.gitlab.cdagaming.craftpresence.handler;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.TextComponentString;
-import org.apache.commons.lang3.text.WordUtils;
 
 import java.lang.reflect.Field;
 import java.net.InetAddress;
@@ -199,8 +198,31 @@ public class StringHandler {
             if (STRIP_COLOR_PATTERN.matcher(formattedKey).find()) {
                 formattedKey = STRIP_COLOR_PATTERN.matcher(formattedKey).replaceAll("");
             }
-            return WordUtils.capitalizeFully(formattedKey);
+            return capitalizeWord(formattedKey);
         }
+    }
+
+    public static String capitalizeWord(String str) {
+        StringBuffer s = new StringBuffer();
+
+        // Declare a character of space
+        // To identify that the next character is the starting
+        // of a new word
+        char ch = ' ';
+        for (int i = 0; i < str.length(); i++) {
+
+            // If previous character is space and current
+            // character is not space then it shows that
+            // current letter is the starting of the word
+            if (ch == ' ' && str.charAt(i) != ' ')
+                s.append(Character.toUpperCase(str.charAt(i)));
+            else
+                s.append(str.charAt(i));
+            ch = str.charAt(i);
+        }
+
+        // Return the string with trimming
+        return s.toString().trim();
     }
 
     public static List<String> splitTextByNewLine(final String original) {
