35c14125fc4eb77a2dbb859724e16e567c6688f5
cdagaming <cstack2011@yahoo.com>
2019-04-18 17:26:07 -0500

    [Change] Initial Page System in ColorEditor
    Also fix Issues with Color Editor Text Erasing when Screen Size Changes

--- src/main/java/com/gitlab/cdagaming/craftpresence/config/gui/ConfigGUI_ColorEditor.java
+++ src/main/java/com/gitlab/cdagaming/craftpresence/config/gui/ConfigGUI_ColorEditor.java
@@ -14,13 +14,15 @@ import java.io.IOException;
 
 public class ConfigGUI_ColorEditor extends GuiScreen {
     private final GuiScreen parentScreen;
+    private int pageNumber;
     private String currentNormalHexValue, currentConvertedHexValue, configValueName, startingHexValue;
     private int currentRed, currentGreen, currentBlue, currentAlpha;
-    private GUIExtendedButton proceedButton;
+    private GUIExtendedButton proceedButton, nextPageButton, previousPageButton;
     private GuiTextField hexText, redText, greenText, blueText, alphaText;
 
     ConfigGUI_ColorEditor(GuiScreen parentScreen, String configValueName) {
         mc = CraftPresence.instance;
+        pageNumber = 0;
         this.parentScreen = parentScreen;
         this.configValueName = configValueName;
 
@@ -37,16 +39,28 @@ public class ConfigGUI_ColorEditor extends GuiScreen {
     public void initGui() {
         Keyboard.enableRepeatEvents(true);
 
+        // Page 1 Items
+        if (pageNumber == 0) {
+            hexText = new GuiTextField(100, mc.fontRenderer, (width / 2) + 3, CraftPresence.GUIS.getButtonY(1) + 15, 180, 20);
+            hexText.setText(startingHexValue);
+
+            redText = new GuiTextField(110, mc.fontRenderer, (width / 2) + 3, CraftPresence.GUIS.getButtonY(3), 180, 20);
+            greenText = new GuiTextField(120, mc.fontRenderer, (width / 2) + 3, CraftPresence.GUIS.getButtonY(4), 180, 20);
+            blueText = new GuiTextField(130, mc.fontRenderer, (width / 2) + 3, CraftPresence.GUIS.getButtonY(5), 180, 20);
+            alphaText = new GuiTextField(140, mc.fontRenderer, (width / 2) + 3, CraftPresence.GUIS.getButtonY(6), 180, 20);
+        }
+
+        // Page 2 Items TODO
+        if (pageNumber == 1) {
+            //
+        }
+
         proceedButton = new GUIExtendedButton(700, 10, (height - 30), 80, 20, Constants.TRANSLATOR.translate("gui.config.buttonMessage.back"));
+        previousPageButton = new GUIExtendedButton(800, (proceedButton.x + proceedButton.getWidth()) + 3, (height - 30), 20, 20, "<");
+        nextPageButton = new GUIExtendedButton(900, (previousPageButton.x + previousPageButton.getWidth()) + 3, (height - 30), 20, 20, ">");
 
-        hexText = new GuiTextField(100, mc.fontRenderer, (width / 2) + 3, CraftPresence.GUIS.getButtonY(1), 180, 20);
-        hexText.setText(startingHexValue);
-
-        redText = new GuiTextField(110, mc.fontRenderer, (width / 2) + 3, CraftPresence.GUIS.getButtonY(3), 180, 20);
-        greenText = new GuiTextField(120, mc.fontRenderer, (width / 2) + 3, CraftPresence.GUIS.getButtonY(4), 180, 20);
-        blueText = new GuiTextField(130, mc.fontRenderer, (width / 2) + 3, CraftPresence.GUIS.getButtonY(5), 180, 20);
-        alphaText = new GuiTextField(140, mc.fontRenderer, (width / 2) + 3, CraftPresence.GUIS.getButtonY(6), 180, 20);
-
+        buttonList.add(previousPageButton);
+        buttonList.add(nextPageButton);
         buttonList.add(proceedButton);
 
         syncValues();
@@ -59,55 +73,77 @@ public class ConfigGUI_ColorEditor extends GuiScreen {
 
         final String mainTitle = Constants.TRANSLATOR.translate("gui.config.title");
         final String subTitle = Constants.TRANSLATOR.translate("gui.config.title.editor.color", configValueName.replaceAll("_", " "));
-
-        final String hexCodeTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.hexcode");
-        final String redTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.redcolorvalue");
-        final String greenTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.greencolorvalue");
-        final String blueTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.bluecolorvalue");
-        final String alphaTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.alphacolorvalue");
-
         final String previewTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.preview");
         final String noticeTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.refresh");
 
         drawString(mc.fontRenderer, mainTitle, (width / 2) - (StringHandler.getStringWidth(mainTitle) / 2), 10, 0xFFFFFF);
         drawString(mc.fontRenderer, subTitle, (width / 2) - (StringHandler.getStringWidth(subTitle) / 2), 20, 0xFFFFFF);
-
-        drawString(mc.fontRenderer, hexCodeTitle, (width / 2) - 130, CraftPresence.GUIS.getButtonY(1) + 5, 0xFFFFFF);
-        drawString(mc.fontRenderer, redTitle, (width / 2) - 130, CraftPresence.GUIS.getButtonY(3) + 5, 0xFFFFFF);
-        drawString(mc.fontRenderer, greenTitle, (width / 2) - 130, CraftPresence.GUIS.getButtonY(4) + 5, 0xFFFFFF);
-        drawString(mc.fontRenderer, blueTitle, (width / 2) - 130, CraftPresence.GUIS.getButtonY(5) + 5, 0xFFFFFF);
-        drawString(mc.fontRenderer, alphaTitle, (width / 2) - 130, CraftPresence.GUIS.getButtonY(6) + 5, 0xFFFFFF);
-
         drawString(mc.fontRenderer, previewTitle, width - 90, height - 30, 0xFFFFFF);
-        drawString(mc.fontRenderer, noticeTitle, (width / 2) - 90, height - 30, 0xFFFFFF);
+        drawString(mc.fontRenderer, noticeTitle, (width / 2) - 90, CraftPresence.GUIS.getButtonY(1) - 5, 0xFFFFFF);
 
-        proceedButton.enabled = !StringHandler.isNullOrEmpty(hexText.getText());
+        // Page 1 Items
+        if (pageNumber == 0) {
+            final String hexCodeTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.hexcode");
+            final String redTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.redcolorvalue");
+            final String greenTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.greencolorvalue");
+            final String blueTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.bluecolorvalue");
+            final String alphaTitle = Constants.TRANSLATOR.translate("gui.config.editorMessage.alphacolorvalue");
 
-        hexText.drawTextBox();
+            drawString(mc.fontRenderer, hexCodeTitle, (width / 2) - 130, CraftPresence.GUIS.getButtonY(1) + 20, 0xFFFFFF);
+            drawString(mc.fontRenderer, redTitle, (width / 2) - 130, CraftPresence.GUIS.getButtonY(3) + 5, 0xFFFFFF);
+            drawString(mc.fontRenderer, greenTitle, (width / 2) - 130, CraftPresence.GUIS.getButtonY(4) + 5, 0xFFFFFF);
+            drawString(mc.fontRenderer, blueTitle, (width / 2) - 130, CraftPresence.GUIS.getButtonY(5) + 5, 0xFFFFFF);
+            drawString(mc.fontRenderer, alphaTitle, (width / 2) - 130, CraftPresence.GUIS.getButtonY(6) + 5, 0xFFFFFF);
 
-        redText.drawTextBox();
-        greenText.drawTextBox();
-        blueText.drawTextBox();
-        alphaText.drawTextBox();
+            hexText.drawTextBox();
 
-        //Draw Preview Rectangle
-        CraftPresence.GUIS.drawGradientRect(300, width - 45, height - 45, width - 5, height - 5, currentConvertedHexValue, currentConvertedHexValue);
+            redText.drawTextBox();
+            greenText.drawTextBox();
+            blueText.drawTextBox();
+            alphaText.drawTextBox();
 
+            proceedButton.enabled = !StringHandler.isNullOrEmpty(hexText.getText());
+
+            CraftPresence.GUIS.drawGradientRect(300, width - 45, height - 45, width - 5, height - 5, currentConvertedHexValue, currentConvertedHexValue);
+        }
+
+        // Page 2 Items TODO
+        if (pageNumber == 1) {
+            //
+        }
+
+        previousPageButton.enabled = pageNumber != 0;
+        nextPageButton.enabled = pageNumber != 1;
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
     @Override
     protected void actionPerformed(GuiButton button) {
+        if (button.id == previousPageButton.id && pageNumber != 0) {
+            pageNumber--;
+            syncValues();
+        }
+        if (button.id == nextPageButton.id && pageNumber != 1) {
+            pageNumber++;
+            syncValues();
+        }
+
         if (button.id == proceedButton.id) {
             syncValues();
 
             if (!StringHandler.isNullOrEmpty(configValueName)) {
-                if (configValueName.equals(CraftPresence.CONFIG.NAME_tooltipBGColor) && !currentNormalHexValue.equals(CraftPresence.CONFIG.tooltipBGColor)) {
-                    CraftPresence.CONFIG.hasChanged = true;
-                    CraftPresence.CONFIG.tooltipBGColor = currentNormalHexValue;
-                } else if (configValueName.equals(CraftPresence.CONFIG.NAME_tooltipBorderColor) && !currentNormalHexValue.equals(CraftPresence.CONFIG.tooltipBorderColor)) {
-                    CraftPresence.CONFIG.hasChanged = true;
-                    CraftPresence.CONFIG.tooltipBorderColor = currentNormalHexValue;
+                if (pageNumber == 0) {
+                    if (configValueName.equals(CraftPresence.CONFIG.NAME_tooltipBGColor) && !currentNormalHexValue.equals(CraftPresence.CONFIG.tooltipBGColor)) {
+                        CraftPresence.CONFIG.hasChanged = true;
+                        CraftPresence.CONFIG.tooltipBGColor = currentNormalHexValue;
+                    } else if (configValueName.equals(CraftPresence.CONFIG.NAME_tooltipBorderColor) && !currentNormalHexValue.equals(CraftPresence.CONFIG.tooltipBorderColor)) {
+                        CraftPresence.CONFIG.hasChanged = true;
+                        CraftPresence.CONFIG.tooltipBorderColor = currentNormalHexValue;
+                    }
+                }
+
+                if (pageNumber == 1) {
+                    // TODO
                 }
             }
 
@@ -121,11 +157,27 @@ public class ConfigGUI_ColorEditor extends GuiScreen {
             mc.displayGuiScreen(parentScreen);
         }
 
-        hexText.textboxKeyTyped(typedChar, keyCode);
-        redText.textboxKeyTyped(typedChar, keyCode);
-        greenText.textboxKeyTyped(typedChar, keyCode);
-        blueText.textboxKeyTyped(typedChar, keyCode);
-        alphaText.textboxKeyTyped(typedChar, keyCode);
+        if (keyCode == Keyboard.KEY_LEFT && pageNumber != 0) {
+            pageNumber--;
+        }
+
+        if (keyCode == Keyboard.KEY_RIGHT && pageNumber != 1) {
+            pageNumber++;
+        }
+
+        // Page 1 Items
+        if (pageNumber == 0) {
+            hexText.textboxKeyTyped(typedChar, keyCode);
+            redText.textboxKeyTyped(typedChar, keyCode);
+            greenText.textboxKeyTyped(typedChar, keyCode);
+            blueText.textboxKeyTyped(typedChar, keyCode);
+            alphaText.textboxKeyTyped(typedChar, keyCode);
+        }
+
+        // Page 2 Items TODO
+        if (pageNumber == 1) {
+            //
+        }
 
         if (keyCode == Keyboard.KEY_NUMPADENTER || keyCode == Keyboard.KEY_RETURN) {
             syncValues();
@@ -134,22 +186,38 @@ public class ConfigGUI_ColorEditor extends GuiScreen {
 
     @Override
     protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-        hexText.mouseClicked(mouseX, mouseY, mouseButton);
-        redText.mouseClicked(mouseX, mouseY, mouseButton);
-        greenText.mouseClicked(mouseX, mouseY, mouseButton);
-        blueText.mouseClicked(mouseX, mouseY, mouseButton);
-        alphaText.mouseClicked(mouseX, mouseY, mouseButton);
+        // Page 1 Items
+        if (pageNumber == 0) {
+            hexText.mouseClicked(mouseX, mouseY, mouseButton);
+            redText.mouseClicked(mouseX, mouseY, mouseButton);
+            greenText.mouseClicked(mouseX, mouseY, mouseButton);
+            blueText.mouseClicked(mouseX, mouseY, mouseButton);
+            alphaText.mouseClicked(mouseX, mouseY, mouseButton);
+        }
+
+        // Page 2 Items TODO
+        if (pageNumber == 1) {
+            //
+        }
 
         super.mouseClicked(mouseX, mouseY, mouseButton);
     }
 
     @Override
     public void updateScreen() {
-        hexText.updateCursorCounter();
-        redText.updateCursorCounter();
-        greenText.updateCursorCounter();
-        blueText.updateCursorCounter();
-        alphaText.updateCursorCounter();
+        // Page 1 Items
+        if (pageNumber == 0) {
+            hexText.updateCursorCounter();
+            redText.updateCursorCounter();
+            greenText.updateCursorCounter();
+            blueText.updateCursorCounter();
+            alphaText.updateCursorCounter();
+        }
+
+        // Page 2 Items TODO
+        if (pageNumber == 1) {
+            //
+        }
     }
 
     @Override
@@ -158,46 +226,54 @@ public class ConfigGUI_ColorEditor extends GuiScreen {
     }
 
     private void syncValues() {
-        Integer localValue = null;
-        Color localColor;
+        // Page 1 - RGBA / Hex Syncing
+        if (pageNumber == 0) {
+            Integer localValue = null;
+            Color localColor;
 
-        if (!StringHandler.isNullOrEmpty(hexText.getText()) && !hexText.getText().equals(currentNormalHexValue)) {
-            if (hexText.getText().startsWith("#") || hexText.getText().length() == 6) {
-                localValue = StringHandler.getColorFromHex(hexText.getText()).getRGB();
-            } else if (hexText.getText().startsWith("0x")) {
-                localColor = new Color(Long.decode(hexText.getText()).intValue(), true);
-                localValue = localColor.getRGB();
-            } else if (StringHandler.isValidInteger(hexText.getText())) {
-                localValue = Integer.decode(hexText.getText());
-            }
+            if (!StringHandler.isNullOrEmpty(hexText.getText())) {
+                if (hexText.getText().startsWith("#") || hexText.getText().length() == 6) {
+                    localValue = StringHandler.getColorFromHex(hexText.getText()).getRGB();
+                } else if (hexText.getText().startsWith("0x")) {
+                    localColor = new Color(Long.decode(hexText.getText()).intValue(), true);
+                    localValue = localColor.getRGB();
+                } else if (StringHandler.isValidInteger(hexText.getText())) {
+                    localValue = Integer.decode(hexText.getText());
+                }
 
-            if (localValue != null) {
-                currentAlpha = (localValue >> 24 & 255);
-                currentRed = (localValue >> 16 & 255);
-                currentGreen = (localValue >> 8 & 255);
-                currentBlue = (localValue & 255);
+                if (localValue != null) {
+                    currentAlpha = (localValue >> 24 & 255);
+                    currentRed = (localValue >> 16 & 255);
+                    currentGreen = (localValue >> 8 & 255);
+                    currentBlue = (localValue & 255);
 
-                alphaText.setText(Integer.toString(currentAlpha));
-                redText.setText(Integer.toString(currentRed));
-                greenText.setText(Integer.toString(currentGreen));
-                blueText.setText(Integer.toString(currentBlue));
+                    alphaText.setText(Integer.toString(currentAlpha));
+                    redText.setText(Integer.toString(currentRed));
+                    greenText.setText(Integer.toString(currentGreen));
+                    blueText.setText(Integer.toString(currentBlue));
 
-                currentNormalHexValue = hexText.getText();
-                currentConvertedHexValue = Integer.toString(localValue);
-            }
-        } else if (!StringHandler.isNullOrEmpty(redText.getText()) && !StringHandler.isNullOrEmpty(greenText.getText()) && !StringHandler.isNullOrEmpty(blueText.getText()) && !StringHandler.isNullOrEmpty(alphaText.getText())) {
-            if (!redText.getText().equals(Integer.toString(currentRed)) || !blueText.getText().equals(Integer.toString(currentBlue)) || !greenText.getText().equals(Integer.toString(currentGreen)) || !alphaText.getText().equals(Integer.toString(currentAlpha))) {
-                if (StringHandler.isValidInteger(redText.getText()) && StringHandler.isValidInteger(greenText.getText()) && StringHandler.isValidInteger(blueText.getText()) && StringHandler.isValidInteger(alphaText.getText())) {
-                    if (!(Integer.parseInt(redText.getText()) > 255 || Integer.parseInt(greenText.getText()) > 255 || Integer.parseInt(blueText.getText()) > 255 || Integer.parseInt(alphaText.getText()) > 255)) {
-                        localColor = new Color(Integer.parseInt(redText.getText()), Integer.parseInt(greenText.getText()), Integer.parseInt(blueText.getText()), Integer.parseInt(alphaText.getText()));
+                    currentNormalHexValue = hexText.getText();
+                    currentConvertedHexValue = Integer.toString(localValue);
+                }
+            } else if (!StringHandler.isNullOrEmpty(redText.getText()) && !StringHandler.isNullOrEmpty(greenText.getText()) && !StringHandler.isNullOrEmpty(blueText.getText()) && !StringHandler.isNullOrEmpty(alphaText.getText())) {
+                if (!redText.getText().equals(Integer.toString(currentRed)) || !blueText.getText().equals(Integer.toString(currentBlue)) || !greenText.getText().equals(Integer.toString(currentGreen)) || !alphaText.getText().equals(Integer.toString(currentAlpha))) {
+                    if (StringHandler.isValidInteger(redText.getText()) && StringHandler.isValidInteger(greenText.getText()) && StringHandler.isValidInteger(blueText.getText()) && StringHandler.isValidInteger(alphaText.getText())) {
+                        if (!(Integer.parseInt(redText.getText()) > 255 || Integer.parseInt(greenText.getText()) > 255 || Integer.parseInt(blueText.getText()) > 255 || Integer.parseInt(alphaText.getText()) > 255)) {
+                            localColor = new Color(Integer.parseInt(redText.getText()), Integer.parseInt(greenText.getText()), Integer.parseInt(blueText.getText()), Integer.parseInt(alphaText.getText()));
 
-                        currentNormalHexValue = StringHandler.getHexFromColor(localColor);
-                        hexText.setText(currentNormalHexValue);
+                            currentNormalHexValue = StringHandler.getHexFromColor(localColor);
+                            hexText.setText(currentNormalHexValue);
 
-                        currentConvertedHexValue = Long.toString(Long.decode(currentNormalHexValue).intValue());
+                            currentConvertedHexValue = Long.toString(Long.decode(currentNormalHexValue).intValue());
+                        }
                     }
                 }
             }
+        }
+
+        // Page 2 - MC Texture Syncing
+        if (pageNumber == 1) {
+            //
         }
     }
 }
