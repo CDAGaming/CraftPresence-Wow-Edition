5d881bbe3a2988223b7a8b064fe84d2e30b7c263
CDAGaming <cstack2011@yahoo.com>
2021-04-08 22:28:15 -0500

    [Change] Adjusted colored_log to be more accessible
    - Timestamps are also now colored for more distinguished layouts

--- Script/colored_log.py
+++ Script/colored_log.py
@@ -13,6 +13,12 @@ MAPPING = {
 
 PREFIX = '\033['
 SUFFIX = '\033[0m'
+DEFAULT_COLOR = 37  # default white
+
+
+def set_as_color(text, color):
+    return '{0}{1}m{2}{3}' \
+        .format(PREFIX, color, text, SUFFIX)
 
 
 class ColoredFormatter(Formatter):
@@ -22,9 +28,12 @@ class ColoredFormatter(Formatter):
 
     def format(self, record):
         colored_record = copy(record)
+        # Format Level Name
         levelname = colored_record.levelname
-        seq = MAPPING.get(levelname, 37)  # default white
-        colored_levelname = '{0}{1}m{2}{3}' \
-            .format(PREFIX, seq, levelname, SUFFIX)
+        seq = MAPPING.get(levelname, DEFAULT_COLOR)
+        colored_levelname = set_as_color(levelname, seq)
         colored_record.levelname = colored_levelname
+        # Format Time if applicable, to be yellow
+        if self.usesTime():
+            self.datefmt = set_as_color(self.datefmt, 33)
         return Formatter.format(self, colored_record)
