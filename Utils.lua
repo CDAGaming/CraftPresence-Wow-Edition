95073ea247050dd24195bf719ece011178ba36ca
CDAGaming <cstack2011@yahoo.com>
2021-08-28 13:55:32 -0500

    [Change] Updated GetFlavorInfo for 9.1.5 Retail PTR
    - Preliminary support, subsequent updates to come if needed.

--- Utils.lua
+++ Utils.lua
@@ -609,7 +609,7 @@ function CraftPresence:GetFlavorInfo()
             ["retail"] = 90100, -- Latest Retail
             ["classic"] = 20501, -- Latest Classic
             ["classic_era"] = 11307, -- Latest Classic Era
-            ["ptr"] = 90100, -- Latest Retail PTR
+            ["ptr"] = 90105, -- Latest Retail PTR
             ["classic_ptr"] = 20502 -- Latest Classic PTR
         }
     end
