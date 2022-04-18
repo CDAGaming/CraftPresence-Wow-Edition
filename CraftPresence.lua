90a526f9a2e03676fad59dec70d68e91038d6238
CDAGaming <cstack2011@yahoo.com>
2022-04-18 15:52:38 -0500

    [Hotfix] Hotfix some cleanups from v1.8.0

--- CraftPresence.lua
+++ CraftPresence.lua
@@ -104,9 +104,9 @@ function CraftPresence:EncodeConfigData(log_output)
     -- Secondary Variable Data
     local rpcData = {
         self:GetFromDb("clientId"),
-        { self:GetFromDb("largeImageKey"), "icon" },
+        self:FormatAsIcon(self:GetFromDb("largeImageKey")),
         self:GetFromDb("largeImageMessage"),
-        { self:GetFromDb("smallImageKey"), "icon" },
+        self:FormatAsIcon(self:GetFromDb("smallImageKey")),
         self:GetFromDb("smallImageMessage"),
         self:GetFromDb("detailsMessage"),
         self:GetFromDb("gameStateMessage")
