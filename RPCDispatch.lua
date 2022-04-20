1bd9782ab141c80777b899668187a0c755037ebb
CDAGaming <cstack2011@yahoo.com>
2022-04-19 19:13:30 -0500

    [Refactor] Misc. Cleanups

--- RPCDispatch.lua
+++ RPCDispatch.lua
@@ -64,8 +64,8 @@ function CraftPresence:GetEncodedMessage(obj, alt, format, level, display)
     level = self:GetOrDefault(level, "%s")
     display = self:GetOrDefault(display, false)
     local output = self:Replace(
-            (type(obj) == "string" and obj) or alt,
-            L["ARRAY_SEPARATOR_KEY"], L["ARRAY_SEPARATOR_KEY_ALT"]
+        (type(obj) == "string" and obj) or alt,
+        L["ARRAY_SEPARATOR_KEY"], L["ARRAY_SEPARATOR_KEY_ALT"]
     )
     if self:GetFromDb("verboseMode") and not self:IsNullOrEmpty(obj) then
         output = self:SerializeTable(obj)
@@ -304,17 +304,17 @@ function CraftPresence:PaintMessageWait(force, update, clean, msg)
             end
         end
         self:GetEncodedMessage(
-                (useTable and encodedArgs), encoded,
-                L["DEBUG_SEND_ACTIVITY"], L["LOG_DEBUG"],
-                self:GetFromDb("debugMode")
+            (useTable and encodedArgs), encoded,
+            L["DEBUG_SEND_ACTIVITY"], L["LOG_DEBUG"],
+            self:GetFromDb("debugMode")
         )
         self:PaintSomething(encoded)
         if clean then
             local delay = self:GetFromDb("frameClearDelay")
             if (self:IsWithinValue(
-                    delay,
-                    max(L["MINIMUM_FRAME_CLEAR_DELAY"], 1), L["MAXIMUM_FRAME_CLEAR_DELAY"],
-                    true, true
+                delay,
+                max(L["MINIMUM_FRAME_CLEAR_DELAY"], 1), L["MAXIMUM_FRAME_CLEAR_DELAY"],
+                true, true
             )) then
                 self:After(delay, function()
                     self:CleanFrames()
