edb35eee2f0715f295fb7ac93e302d23f1ceb462
CDAGaming <cstack2011@yahoo.com>
2022-04-30 21:40:56 -0500

    [Fix] Fixed Zereth Mortis Support for `player_alliance`
    - Turns out, Zereth Mortis is a different continent, and thus was never supported until now.

--- Config.lua
+++ Config.lua
@@ -424,6 +424,7 @@ end]],
     -- Covenant and/or Faction data is only updated if the instance is changed
     if (playerCovenantId == 0 or not (
             (self:FindMatches(name, 'Shadowlands', false, 1, false)) or
+                    (self:FindMatches(name, 'Zereth Mortis', false, 1, false)) or
                     (self:FindMatches(name, 'Torghast', false, 1, false)) or
                     (self:FindMatches(self:GetCurrentInstanceTier(), 'Shadowlands', false, 1, false))
     )) then
