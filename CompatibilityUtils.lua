0ffffc2fede4b13169f75c125ee17aec2de10535
CDAGaming <cstack2011@yahoo.com>
2023-03-22 11:16:20 -0500

    [Change] Bumped TOC + Flavor Info for 10.0.7 Retail + 10.1 PTR

--- CompatibilityUtils.lua
+++ CompatibilityUtils.lua
@@ -35,7 +35,7 @@ function CraftPresence:GetFlavorInfo(key, value)
     if not self.cache.flavors then
         self.cache.flavors = {
             ["retail"] = {
-                ["toc"] = 100005,
+                ["toc"] = 100007,
                 ["name"] = "World of Warcraft",
                 ["product_id"] = "retail"
             },
@@ -50,12 +50,12 @@ function CraftPresence:GetFlavorInfo(key, value)
                 ["product_id"] = "classic_era"
             },
             ["ptr"] = {
-                ["toc"] = 100005,
+                ["toc"] = 100100,
                 ["name"] = "Public Test Realm",
                 ["product_id"] = "ptr"
             },
             ["beta"] = {
-                ["toc"] = 100005,
+                ["toc"] = 100100,
                 ["name"] = "Dragonflight Beta",
                 ["product_id"] = "beta"
             },
