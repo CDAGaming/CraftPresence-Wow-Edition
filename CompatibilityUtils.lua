0b893537acc491f26491921da141adf4491a1ee0
CDAGaming <cstack2011@yahoo.com>
2024-03-21 14:57:58 -0500

    [Change] Bumped TOC + Flavor Info for Retail 10.2.6

--- CompatibilityUtils.lua
+++ CompatibilityUtils.lua
@@ -35,7 +35,7 @@ function CraftPresence:GetFlavorInfo(key, value)
     if not self.cache.flavors then
         self.cache.flavors = {
             ["retail"] = {
-                ["toc"] = 100205,
+                ["toc"] = 100206,
                 ["name"] = "World of Warcraft",
                 ["product_id"] = "retail"
             },
